afc5830e0e9024c450c788a559df701be2a68e97
suenos89 <Melankohlroulade@Melankohlroulad>
2014-05-10 21:55:53 +0200

    ?

--- SocialMediaPinnwand/src/hdm/social/media/pinnwand/server/db/test.java
+++
@@ -1,9 +0,0 @@
-package hdm.social.media.pinnwand.server.db;
-
-public class test {
-
-	public static void main(String[] args) {
-	System.out.println(BeitragMapper.beitragMapper().getBeitragById(1).getInhalt());
-	}
-
-}
